3b51f986c8c9035cc9e1d2abd823671026666af6
magwach <emmanuelmagothe@gmail.com>
2024-06-21 17:17:30 +0300

    updated the code

--- script.js
+++ script.js
@@ -21,13 +21,42 @@ function isInvalidInput(str)
 function addEntry()
 {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll(input[type="text"]).length;
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString = `
-    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-    <input placeholder= "Name" id="${entryDropdown.value}-${entryNumber}-name" type="text" ></input>
-     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-     <input type = "number" min ="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"></input>
+   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories"/>
      `;
-
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
+function getCaloriesFromInputs(list)
+{
+    let calories = 0;
+    for (const item of list)
+        {
+            const currVal = cleanInputString(item.value);
+            const invalidInputMatch = isInvalidInput(currVal);
+            if (invalidInputMatch)
+                {
+                    alert(`Invalid Input: ${invalidInputMatch[0]}`);
+                    isError = true;
+                    return null;
+                }
+            calories += Number(currVal);
+        }
+        return calories;
+}
+
+function calculateCalories(e)
+{
+    let breakfastNumberInputs = document.querySelectorAll("#breakfast input[type=number]");
+    let lunchNumberInputs = document.querySelectorAll("#lunch input[type=number]");
+    let dinnerNumberInputs = document.querySelectorAll("#dinner input[type=number]");
+    let snacksNumberInputs = document.querySelectorAll("#snacks input[type=number]");
+    let exerciseNumberInputs = document.querySelectorAll("#exercise input[type=number]");
+    e.preventDefault();
+    isError = false;
+}
+addEntryButton.addEventListener('click', addEntry);
